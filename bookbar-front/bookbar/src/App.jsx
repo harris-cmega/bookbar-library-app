29bd8fe72d05bd5fec6db92f8ebacd84fddb255b
kossovar <qmgharris@icloud.com>
2024-04-12 23:50:52 +0200

    create CTA (component)

--- bookbar-front/bookbar/src/App.jsx
+++ bookbar-front/bookbar/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar";
+import CallToAction from './components/CallToAction';
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <CallToAction />
     </>
   )
 }
---
+++ bookbar-front/bookbar/src/components/CallToAction.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CallToAction = () => {
+  return (
+    <div className="bg-img-hero" style={{backgroundColor: '#3D5962' }}>
+      <div className="container text-center text-lg-left space-2" style={{ position: 'relative' }}>
+        <div className="row align-items-lg-center">
+          <div className="col-lg-12 mb-5 p-3 mb-lg-0">
+            <p className="lead text-light mb-0">Premium Members Get 10% Off and Earn Rewards | <a href='#' style={{color: 'inherit'}}>Find out more!</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CallToAction
