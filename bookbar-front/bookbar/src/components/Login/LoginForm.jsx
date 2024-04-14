56d564390b89acc4601ba114e331de587bbc0152
ArditMirenaa <arditm08@gmail.com>
2024-04-14 14:29:56 +0200

    Login fix

--- bookbar-front/bookbar/src/components/Login/LoginForm.jsx
+++ bookbar-front/bookbar/src/components/Login/LoginForm.jsx
@@ -1,36 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import loginpageimage from '../../assets/login-page-image.jpg';
 
 const LoginForm = () => {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-12 col-lg-5">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="card-title">Log In into your Account!</div>
-                            <form action="" method="post" novalidate id="userloginform">
-                                <div className="form-floating">
-                                    <input type="email" id="email" className="form-control" placeholder="Place Email here" required />
-                                    <label htmlFor="email">Email</label>
-                                    <input type="password" id="password" className="form-control" placeholder="Enter Password here" required />
-                                    <label htmlFor="password">Password</label>
+        <section class="vh-100">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12 col-lg-5 col-sm-5 text-black">
+                        <div className="d-flex align-items-center h-custom-2 px-4 ms-xl-4 mt-auto pt-auto pt-xl-0 mt-xl-n5">
+                            <form style={{ width: '23rem' }} noValidate id="userloginform">
+
+                                <h3 className="fw-normal mb-3 pb-3" style={{ letterSpacing: '1px' }}>Log In</h3>
+
+                                <div className="form-outline mb-4">
+                                    <input type="email" id="" className="form-control form-control-lg" required/>
+                                    <label className="form-label" htmlFor="email">Email</label>
                                 </div>
-                                <button type="submit" className='btn btn-primary'>Submit</button>
+
+                                <div className="form-outline mb-4">
+                                    <input type="password" id="" className="form-control form-control-lg" />
+                                    <label className="form-label" htmlFor="password">Password</label>
+                                </div>
+
+                                <div className="pt-1 mb-4">
+                                    <button className="btn btn-primary btn-lg btn-login btn-block" type="submit">Login</button>
+                                </div>
+                                <p>Don't have an account? <Link to="/signup" className="link-primary">Register here</Link></p>
+
                             </form>
                         </div>
                     </div>
-                    <div className="card">
-                        <div className="card-text">
-                            <Link className="btn btn-secondary" to="/signup">Create an Account</Link>
-                        </div>
+                    <div className="col-12 col-lg-7 col-sm-7 px-0 d-none d-sm-block">
+                        <img src={loginpageimage} alt="Login image" className="w-100 vh-100" style={{ objectFit: 'cover', objectPosition: 'left' }} />
                     </div>
                 </div>
-                <div className="col-12 col-lg-7">
-                    <img src="./assets/login-page-image.jpg" alt="Login Page Image" />
-                </div>
             </div>
-        </div>
+        </section>
     );
 };
 
