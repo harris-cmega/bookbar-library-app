dcb302da4912f0c6278d30346c680e5ee5b07685
BlendiGjoni <bg64715@ubt-uni.net>
2024-06-03 17:28:20 +0200

    Create Cart on User Register fix

--- src/main/java/io/bookbar/bookbarbackend/entities/Cart.java
+++ src/main/java/io/bookbar/bookbarbackend/entities/Cart.java
@@ -3,11 +3,14 @@ package io.bookbar.bookbarbackend.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Entity
 @Table(name = "cart")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cart {
 
     @Id
@@ -19,5 +22,5 @@ public class Cart {
     private User user;
 
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<CartItem> cartItems;
+    private List<CartItem> cartItems = new ArrayList<>();
 }
